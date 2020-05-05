298769b63b505ad08fe7240c4edaee515abeb295
tommynguyen <tntt_tom@me.com>
2020-05-05 17:17:28 -0500

    Refactored
    Great Redux Practice + Visualization

--- src/index.js
+++ src/index.js
@@ -4,49 +4,24 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { createStore } from "redux";
+import { Provider } from "react-redux";
 
-//STORE -> GLOBALIZED STATE
+import rootReducer from "./reducers/";
 
-//ACTION INCREMENT
-const increment = () => {
-  return {
-    type: "INCREMENT"
-  };
-};
-
-const decrement = () => {
-  return {
-    type: "DECREMENT"
-  };
-};
-
-//REDUCER
-const counter = (state = 0, action) => {
-  switch (action.type) {
-    case "INCREMENT":
-      return state + 1;
-
-    case "DECREMENT":
-      return state - 1;
-  }
-};
-
-let store = createStore(counter);
-
-//CONSOLE DISPLAY
-store.subscribe(() => console.log(store.getState()));
-
-//DISPATCH
-store.dispatch(increment());
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
-);
+); // If you want your app to work offline and load faster, you can change
 
-// If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
