1cd8c11ba5c0a6f76fe5625799780bcdf4ede791
tommynguyen <tntt_tom@me.com>
2020-05-05 15:03:10 -0500

    Created store, action, reducer, and dispatch

--- src/index.js
+++ src/index.js
@@ -3,8 +3,41 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+import { createStore } from "redux";
 
-//Store
+//STORE -> GLOBALIZED STATE
+
+//ACTION INCREMENT
+const increment = () => {
+  return {
+    type: "INCREMENT"
+  };
+};
+
+const decrement = () => {
+  return {
+    type: "DECREMENT"
+  };
+};
+
+//REDUCER
+const counter = (state = 0, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return state + 1;
+
+    case "DECREMENT":
+      return state - 1;
+  }
+};
+
+let store = createStore(counter);
+
+//CONSOLE DISPLAY
+store.subscribe(() => console.log(store.getState()));
+
+//DISPATCH
+store.dispatch(increment());
 
 ReactDOM.render(
   <React.StrictMode>
